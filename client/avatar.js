bdfb41c6baa93af8ae80025edad958cebb5da36d
Ardnived <devindra@shaw.ca>
2014-08-07 15:25:04 -0700

    Refactor avatar.

--- client/avatar.js
+++ client/avatar.js
@@ -23,7 +23,7 @@ var avatar = {
 			.gravityConst(0.4)
 			.bind('NewDirection', this.on_change_direction)
 			.bind('Moved', this.on_moved)
-			.bind('EnterFrame', this.on_enter_frame)
+			.bind('EnterFrame', this.update)
 			.bind('KeyDown', this.on_key_down)
 			.bind('KeyUp', this.on_key_up);
 
@@ -78,6 +78,74 @@ var avatar = {
 		document.getElementById('observerHint').style.display = ''
 		Crafty.viewport.mouselook(true);
 	},
+	update: function() {
+		//move colliding movable objects
+		/*var hitDetection = this.hit('Movable');
+		if (hitDetection){
+			if (this.isDown('RIGHT_ARROW'))
+				hitDetection[0].obj.x += 4;
+			else if (this.isDown('LEFT_ARROW'))
+				hitDetection[0].obj.x -= 4;
+		}*/
+
+		avatar.update_dash();
+		avatar.update_shield();
+	},
+	use_dash: function() {
+		debug.game("Activate Dash");
+		if (avatar.direction == 'East') {
+			avatar.entity.x += 50;
+		} else if (avatar.direction == 'West') {
+			avatar.entity.x -= 50;
+		} else {
+			avatar.entity.y -= 100;
+			debug.warn("Default to dash up.");
+		}
+
+		avatar.lastDash = new Date();
+		avatar.dashCountdown = true;
+	},
+	update_dash: function() {
+		if (avatar.dashCountdown) {
+			var cooldown = (DASH_COOLDOWN - (new Date() - avatar.lastDash));
+			if (cooldown <= 0){
+				avatar.dashCountdown = false;
+				document.getElementById('dashText').innerHTML = ("DASH READY");
+			} else {
+				document.getElementById('dashText').innerHTML = ("NEXT DASH: " + cooldown/1000);
+			}
+		}
+	},
+	use_shield: function() {
+		debug.game("Activate Shield");
+		avatar.shieldUp = true;
+		avatar.lastShield = new Date();
+		avatar.shieldCountdown = true;
+		avatar.shield.visible = true;
+	},
+	update_shield: function() {
+		if (avatar.shieldCountdown) {
+			if (avatar.shieldUp) {
+				var countdown = (SHIELD_DURATION - (new Date() - avatar.lastShield));
+
+				if (countdown <= 0) {
+					avatar.shield.visible = false;
+					avatar.shieldUp = false;
+					avatar.lastShield = new Date();
+				} else {
+					document.getElementById('shieldText').innerHTML = ("SHIELD: " + countdown/1000);
+				}
+			} else {
+				var countdown = SHIELD_COOLDOWN - (new Date() - avatar.lastShield);
+				if (countdown <= 0) {
+					avatar.shieldCountdown = false;
+					document.getElementById('shieldText').innerHTML = ("SHIELD READY");
+				} else {
+					document.getElementById('shieldText').innerHTML = ("NEXT SHIELD: " + countdown/1000);
+				}
+			}
+		}
+	},
 	check_deathzones: function() {
 		if (avatar.shieldUp) {
 			// We are shielded and can't be hurt.
@@ -110,50 +178,6 @@ var avatar = {
 		}
 
 		return false;
-	},
-	on_enter_frame: function() {
-		//move colliding movable objects
-		var hitDetection = this.hit('Movable');
-		if (hitDetection){
-			if (this.isDown('RIGHT_ARROW'))
-				hitDetection[0].obj.x += 4;
-			else if (this.isDown('LEFT_ARROW'))
-				hitDetection[0].obj.x -= 4;
-		}
-
-		//dash cooldown
-		if (avatar.dashCountdown) {
-			var cooldown = (DASH_COOLDOWN - (new Date() - avatar.lastDash));
-			if (cooldown <= 0){
-				avatar.dashCountdown = false;
-				document.getElementById('dashText').innerHTML = ("DASH READY");
-			} else {
-				document.getElementById('dashText').innerHTML = ("NEXT DASH: " + cooldown/1000);
-			}
-		}
-
-		//shield countdown
-		if (avatar.shieldCountdown) {
-			if (avatar.shieldUp) {
-				var countdown = (SHIELD_DURATION - (new Date() - avatar.lastShield));
-
-				if (countdown <= 0) {
-					avatar.shield.visible = false;
-					avatar.shieldUp = false;
-					avatar.lastShield = new Date();
-				} else {
-					document.getElementById('shieldText').innerHTML = ("SHIELD: " + countdown/1000);
-				}
-			} else {
-				var countdown = SHIELD_COOLDOWN - (new Date() - avatar.lastShield);
-				if (countdown <= 0) {
-					avatar.shieldCountdown = false;
-					document.getElementById('shieldText').innerHTML = ("SHIELD READY");
-				} else {
-					document.getElementById('shieldText').innerHTML = ("NEXT SHIELD: " + countdown/1000);
-				}
-			}
-		}
 	},
 	on_change_direction: function(event) {
 		if (this.isDown('LEFT_ARROW')) {
@@ -188,22 +212,10 @@ var avatar = {
 	on_key_down: function(e) {
 		//dash
 		if (e.key == Crafty.keys.C && !avatar.dashCountdown) {
-			if (avatar.direction == 'East') {
-				this.x += 50;
-			} else if (avatar.direction == 'West') {
-				this.x -= 50;
-			} else {
-				debug.error("No direction set");
-			}
-
-			avatar.lastDash = new Date();
-			avatar.dashCountdown = true;
+			avatar.use_dash();
 		//shield
 		} else if (e.key == Crafty.keys.X && !avatar.shieldCountdown){
-			avatar.shieldUp = !avatar.shieldUp;
-			avatar.lastShield = new Date();
-			avatar.shieldCountdown = true;
-			avatar.shield.visible = true;
+			avatar.use_shield();
 		}
 	},
 	on_key_up: function(e){
