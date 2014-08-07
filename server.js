02a364e2ade1609c61b96425078554110d755c09
Devindra Payment <devindra@athinkingape.com>
2014-08-06 20:58:30 -0700

    Added rooms.

--- server.js
+++ server.js
@@ -1,39 +1,7 @@
 var dispatch = require("./server/dispatch");
-//var logic = require("./server/logic");
 var debug = require("./shared/debug");
 
 // Initialize the server.
 dispatch.start(8081);
 
-/**
- * When a new user connects, handle it.
- */
-var clients = {};
-var autoinc = 0;
-
-dispatch.io.on('connection', function(socket) {
-	clients[socket] = {
-		name: "",
-		player_id: autoinc
-	};
-	autoinc++;
-	
-	debug.dispatch("Received Game Connection. Player ID:", clients[socket].player_id);
-	
-	socket.emit('meta', { player_id: clients[socket].player_id });
-	socket.on('meta', function(data) {
-		clients[socket].name = data.name;
-	});
-	
-	socket.on('move', function(data) {
-		socket.broadcast.emit('move', data);
-	});
-	
-	socket.on('stop', function(data) {
-		socket.broadcast.emit('stop', data);
-	});
-	
-	socket.on('disconnect', function () {
-	    // Do Nothing?
-	});
-});
+require("./server/socket");
---
+++ server/socket.js
@@ -0,0 +1,45 @@
+var dispatch = require("../server/dispatch");
+var debug = require("../shared/debug");
+
+/**
+ * When a new user connects, handle it.
+ */
+var clients = {};
+var autoinc = 0;
+
+dispatch.io.on('connection', function(socket) {
+	clients[socket] = {
+		name: "",
+		player_id: autoinc
+	};
+	autoinc++;
+	
+	debug.dispatch("Received Game Connection. Player ID:", clients[socket].player_id);
+	
+	// ------------------
+	var room = "room00";
+	socket.join(room);
+
+	socket.emit('meta', { player_id: clients[socket].player_id });
+	socket.on('meta', function(data) {
+		clients[socket].name = data.name;
+	});
+	
+	socket.on('move', function(data) {
+		socket.broadcast.to(room).emit('move', data);
+	});
+	
+	socket.on('stop', function(data) {
+		socket.broadcast.to(room).emit('stop', data);
+	});
+
+	socket.on('enter', function(data) {
+		socket.leave(room);
+		room = data.room;
+		socket.join(room);
+	})
+	
+	socket.on('disconnect', function () {
+	    // Do Nothing?
+	});
+});
