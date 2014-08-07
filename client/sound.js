f633808396d686f52f0d95b1bf55d0cbb007ef54
Jingyu William Wang <jingyuw@athinkingape.com>
2014-08-07 12:35:21 -0700

    background sound

---
+++ client/sound.js
@@ -0,0 +1,6 @@
+// Initalization
+Crafty.audio.add('sound_background', ['../resources/sounds/background.mp3', '../resources/sounds/background.ogg']);
+
+
+
+//Crafty.audio.play('sound_background', -1);
