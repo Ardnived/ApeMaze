c1e18b155c3452e651d4aeb03562204383025952
Alvin Lao <alvinlao@athinkingape.com>
2014-08-07 11:09:51 -0700

    Add fire switch

--- client/board.js
+++ client/board.js
@@ -10,27 +10,32 @@ var direction = {
 var board = {
 	display: null,
 	init: function() {
-		this.display = canvas.display.line({
-			x: canvas.width / 2,
-			y: canvas.height / 2
-		});
-		this.display.add();
+		Crafty.e("2D, Canvas, TiledMapBuilder")
+			.setMapDataSource( SOURCE_FROM_TILED_MAP_EDITOR )
+			.createWorld(function(map) {
+				console.log("Building tile map");				
+
+				// Floor
+				for(var floor = 0; floor < map.getEntitiesInLayer("floor").length; ++floor) {
+					map.getEntitiesInLayer("floor")[floor]
+						.addComponent("Floor");				
+				}
+
+				// Fire trap
+				for(var fire = 0; fire < map.getEntitiesInLayer("fire_switch").length; ++fire) {
+					if(player.is_controller) {
+						map.getEntitiesInLayer("fire_switch")[fire]
+							.addComponent("2D")
+							.visible = false;
+					}
+				}
+			});
+
+		Crafty.e("2D, Canvas, Color, Movable, Gravity")
+			.attr({x: 100, y: 100, w: 32, h: 32})
+			.color('lightblue')
+			.gravity('Floor');
+			
+		trap.create(0);
 	}
 };
-
-Crafty.e("2D, Canvas, TiledMapBuilder")
-	.setMapDataSource( SOURCE_FROM_TILED_MAP_EDITOR )
-	.createWorld(function(map) {
-		console.log("Building tile map");
-		for(var floor = 0; floor < map.getEntitiesInLayer("floor").length; ++floor) {
-			map.getEntitiesInLayer("floor")[floor]
-				.addComponent("Floor");
-		}
-	});
-
-Crafty.e("2D, Canvas, Color, Movable, Gravity")
-	.attr({x: 100, y: 100, w: 32, h: 32})
-	.color('lightblue')
-	.gravity('Floor');
-	
-trap.create(0);
