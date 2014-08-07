2b46afc1538245e6638275af33f0ebf0ad4af496
Ardnived <devindra@shaw.ca>
2014-08-07 01:47:34 -0700

    Cleaned up html.

--- client/chat.js
+++ client/chat.js
@@ -1,9 +1,3 @@
-chat_module = document.getElementById('chat');
-chat_module.innerHTML = '<p>Message</p>'
-chat_module.innerHTML += '<textarea id="chat_module_content" cols="40" rows="10" readonly></textarea><p></p>'
-chat_module.innerHTML += '<input id="chat_module_text" size="50" onkeypress="return char_module_keypress(event)"></input>'
-chat_module.innerHTML += '<button onclick="chat_module_send()" id="chat_module_submit" cols="40">Submit</button>'
-
 function chat_module_send() {
 	var chat_message = document.getElementById('chat_module_text').value
 	dispatch.emit('chat', {
