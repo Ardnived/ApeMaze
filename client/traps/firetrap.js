9ca1bb863c5c32c69f12300da28fd93c1705255e
Ardnived <devindra@shaw.ca>
2014-08-07 22:23:27 -0700

    Fixed falling blocks.

--- client/traps/firetrap.js
+++ client/traps/firetrap.js
@@ -2,7 +2,7 @@ function FireTrap(id, trigger, threshold) {
 	Trap.call(this, id, trigger, threshold);
 
 	var centerX = (trigger.x + trigger.w) / 2;
-	35/2
+	
 	this.flame = Crafty.e("2D, Canvas, SpriteAnimation, FireSprite, Deathzone")
 					.attr({x: trigger.x, y: trigger.y - 35, w: 35, h: 35})
 					.reel('Burning', 600, 0, 0, 6)
